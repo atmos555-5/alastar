a43fee344e33fcf54a444cb2cf99cbcc37c8073f
atmos <mollyeatkins05@gmail.com>
2026-02-27 18:51:42 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ import Textarea from "react-textarea-autosize";
 import Image from "next/image";
 
 const examples = [
-  "Example 1",
-  "Example 2",
-  "Example 3",
+  "Hi. How are you?",
+  "How's the weather?",
+  "what's your favorite part about being a human?",
 ];
 
 export default function Chat() {
@@ -83,25 +83,8 @@ export default function Chat() {
               className="h-20 w-20"
             />
             <h1 className="text-lg font-semibold text-black">
-              Hi, I'm a fine tuned LLM.
+                Hi, I'm Alastar.
             </h1>
-            <p className="text-gray-500">
-              I'm part of a series of computational experiments taught by{" "}
-              <a
-                href="https://linkin.bio/yallahalim/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="font-medium underline underline-offset-4 transition-colors hover:text-black"
-              >
-                Halim Madi
-              </a>. I was built using{" "}
-              <a
-                href="https://openai.com/blog/gpt-3-5-turbo-fine-tuning-and-api-updates"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="font-medium underline underline-offset-4 transition-colors hover:text-black"
-              >
-                fine-tuned GPT4.
               </a>
             </p>
           </div>
@@ -172,7 +155,7 @@ export default function Chat() {
             href="https://sdk.vercel.ai/docs"
             target="_blank"
             rel="noopener noreferrer"
-            className="transition-colors hover:text-black"
+            className="transition-colors hover:text-pink"
           >
             Vercel AI SDK
           </a>
@@ -181,7 +164,7 @@ export default function Chat() {
             href="https://openai.com/blog/gpt-3-5-turbo-fine-tuning-and-api-updates"
             target="_blank"
             rel="noopener noreferrer"
-            className="transition-colors hover:text-black"
+            className="transition-colors hover:text-pink"
           >
             OpenAI GPT-3.5-turbo, as part of a course taught by
           </a>{" "}
@@ -190,7 +173,7 @@ export default function Chat() {
             href="https://linkin.bio/yallahalim/"
             target="_blank"
             rel="noopener noreferrer"
-            className="transition-colors hover:text-black"
+            className="transition-colors hover:text-pink"
           >
             Learn to build your own
           </a>
